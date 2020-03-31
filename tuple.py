2da2232ead7d8739dbc8241b6095442c06bf154e
baeksangha <35217274+baeksangha@users.noreply.github.com>
2020-03-31 18:23:50 +0900

    Update tuple.py

--- tuple.py
+++ tuple.py
@@ -4,6 +4,8 @@ Counter : dictionary 형태로 list의 원소의 갯수를 표현해줌
 충격,..
 '''
 
+# KAKAO winter internship coding test
+
 def solution(s):
     s = Counter(re.findall('\d+', s))
     return list(map(int, [k for k, v in sorted(s.items(), key=lambda x: x[1], reverse=True)]))
